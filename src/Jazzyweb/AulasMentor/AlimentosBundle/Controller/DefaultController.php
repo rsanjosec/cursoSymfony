8070375b0a7bd77f06caad89383663019ccc5128
cirus <cirus@ubuntu>
2013-05-29 20:30:39 +0200

    Modicicacion del controladr principal para la carga de la plantilla twig de inicio. creacion de los ficheos css y enlaces simbólicos Modificacion delos routing para que carge la aplicacion a partir de /alimentos

--- src/Jazzyweb/AulasMentor/AlimentosBundle/Controller/DefaultController.php
+++ src/Jazzyweb/AulasMentor/AlimentosBundle/Controller/DefaultController.php
@@ -8,8 +8,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class DefaultController extends Controller
 {
     
-    public function indexAction($name)
-    {
-        return $this->render('JazzywebAulasMentorAlimentosBundle:Default:index.html.twig', array('name' => $name));
-    }
+    public function indexAction()
+     {
+         $params = array(
+             'mensaje' => 'Bienvenido al curso de Symfony2',
+             'fecha' => date('d-m-yy'),
+         );
+
+         return
+           $this->render('JazzywebAulasMentorAlimentosBundle:Default:index.html.twig', $params);
+     }
 }
